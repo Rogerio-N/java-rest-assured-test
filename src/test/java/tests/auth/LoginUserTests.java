8baff779da15de773923ce9369bf5fe25f3183f3
Rogerio-N <rogenakayama@gmail.com>
2025-06-12 08:10:13 -0300

    Created login user error tests

--- src/test/java/tests/auth/LoginUserTests.java
+++ src/test/java/tests/auth/LoginUserTests.java
@@ -34,4 +34,38 @@ public class LoginUserTests extends RestAssuredBase {
                 .body("user.uuid", notNullValue());
     }
 
+    @Test
+    @DisplayName("Should have an Unauthorized error when provided with a invalid username and password combination")
+    void wrongParams() {
+        JSONObject request = new JSONObject();
+        request.put("type", "LOGIN");
+        request.put("password", defaultPassword+"wrongValue");
+        request.put("username", defaultUsername);
+
+        given()
+            .request()
+                .body(request.toString())
+        .when()
+            .post("/login")
+        .then()
+            .statusCode(401);
+    }
+
+    @Test
+    @DisplayName("Should have a BadRequest error when provided with a empty param to login")
+    void emptyParam() {
+        JSONObject request = new JSONObject();
+        request.put("type", "LOGIN");
+        request.put("password", defaultPassword);
+        request.put("username", "");
+
+        given()
+            .request()
+                .body(request.toString())
+        .when()
+            .post("/login")
+        .then()
+            .statusCode(400);
+    }
+
 }
